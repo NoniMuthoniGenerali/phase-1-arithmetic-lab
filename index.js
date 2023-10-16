f395aae3f96c3916b7e0159db88b9e504796a6da
NoniMuthoni <noni.muthoni.noni@gmail.com>
2023-10-16 11:06:57 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+var num1 =62
+var num2 =1;
 
-// Write your code here
+var multiply = num1 * num2
+let random = Math.floor(Math.random() *10) + 1 ;
+  
+let num3 = 22
+let num4 = 9
+
+let mod = num3 / num4
+
+let max = Math.max(1,7,5,9,15,20)
